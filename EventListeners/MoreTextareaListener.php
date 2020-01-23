64f7ee7b8ccc0f6f543b82e055d2fadc413a8ef7
Chevrier <anthony@meedle.fr>
2020-01-23 10:34:18 +0100

    mise a jour sauvegarde content folder
    les enregistrement des champs contenu et dosser n'était pas prévu, c'est ajouté.

--- EventListeners/MoreTextareaListener.php
+++ EventListeners/MoreTextareaListener.php
@@ -6,7 +6,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Thelia\Action\BaseAction;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\Event\Product\ProductUpdateEvent;
+use Thelia\Core\Event\Content\ContentUpdateEvent;
 use Thelia\Core\Event\Category\CategoryUpdateEvent;
+use Thelia\Core\Event\Folder\FolderUpdateEvent;
 use MoreTextarea\Model\MoretextareaQuery;
 use MoreTextarea\Model\Moretextarea;
 use MoreTextarea\Model\CategoryMoretextareaQuery;
@@ -42,12 +44,35 @@ class MoreTextareaListener extends BaseAction implements EventSubscriberInterfac
 			$more->setValue($val)->save();
 		}
     }
+    public function content(ContentUpdateEvent $event)
+    {
+		foreach($_REQUEST['moretextarea'] as $idmore => $val){
+			if(null === $more = ContentMoretextareaQuery::create()->filterByLocale($event->getLocale())->filterByContentId($event->getContentId())->filterByMoretextareaId($idmore)->findOne()){
+				$more = new ContentMoretextarea();
+				$more->setContentId($event->getContentId())->setMoretextareaId($idmore)->setLocale($event->getLocale());
+			}
+			$more->setValue($val)->save();
+		}
+    }
+
+    public function folder(FolderUpdateEvent $event)
+    {
+		foreach($_REQUEST['moretextarea'] as $idmore => $val){
+			if(null === $more = FolderMoretextareaQuery::create()->filterByLocale($event->getLocale())->filterByFolderId($event->getFolderId())->filterByMoretextareaId($idmore)->findOne()){
+				$more = new FolderMoretextarea();
+				$more->setFolderId($event->getFolderId())->setMoretextareaId($idmore)->setLocale($event->getLocale());
+			}
+			$more->setValue($val)->save();
+		}
+    }
 
     public static function getSubscribedEvents()
     {
         return array(
             TheliaEvents::PRODUCT_UPDATE => array('product', 128),
-            TheliaEvents::CATEGORY_UPDATE => array('category', 128)
+            TheliaEvents::CATEGORY_UPDATE => array('category', 128),
+            TheliaEvents::FOLDER_UPDATE => array('folder', 128),
+            TheliaEvents::CONTENT_UPDATE => array('content', 128)
         );
     }
 }
